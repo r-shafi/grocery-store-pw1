ae8aa648dc4f4fd7b4ab59725b6e3e308386aa8a
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-13 13:50:30 +0600

    create new routes

--- app/(pages)/_layout.tsx
+++ app/(pages)/_layout.tsx
@@ -1,16 +1,16 @@
 import { Stack } from 'expo-router';
 import React from 'react';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Stack
       screenOptions={{
         headerShown: false,
       }}
-    />
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="search" />
+      <Stack.Screen name="product" />
+    </Stack>
   );
 }
