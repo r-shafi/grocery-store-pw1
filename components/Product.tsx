0283d45718c4c1df722fdb830e57362eeff3ad96
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-13 13:50:48 +0600

    navigation

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,41 +1,50 @@
-import { Image, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 const Product = ({ product }: { product: Product }) => {
+  const navigation = useNavigation();
+
+  const navigate = () => {
+    navigation.navigate('product', { product_id: product.id });
+  };
+
   return (
-    <View
-      style={{
-        backgroundColor: '#3333',
-        width: 150,
-        height: 200,
-        marginHorizontal: 8,
-        position: 'relative',
-      }}
-    >
-      <Image
-        source={{ uri: product.image }}
+    <TouchableOpacity onPress={navigate}>
+      <View
         style={{
+          backgroundColor: '#3333',
           width: 150,
           height: 200,
-          position: 'absolute',
-          top: 0,
-          left: 0,
-        }}
-      />
-      <Text
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          padding: 8,
-          color: 'white',
-          fontSize: 16,
-          backgroundColor: '#0005',
-          width: '100%',
+          marginHorizontal: 8,
+          position: 'relative',
         }}
       >
-        {product.name} - ${product.price}
-      </Text>
-    </View>
+        <Image
+          source={{ uri: product.image }}
+          style={{
+            width: 150,
+            height: 200,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}
+        />
+        <Text
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            padding: 8,
+            color: 'white',
+            fontSize: 16,
+            backgroundColor: '#0005',
+            width: '100%',
+          }}
+        >
+          {product.name} - ${product.price}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
