bbd49fbe5885071d80bf46215925369efd959ddf
Shafi Rayhan <rayhanshafi7@gmail.com>
2025-01-13 12:17:26 +0600

    add types

---
+++ models/products.d.ts
@@ -0,0 +1,12 @@
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  quantity: number;
+  unit: string;
+  description: string;
+  category_id: number;
+  created_at: string;
+  updated_at: string;
+}
